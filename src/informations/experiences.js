449efde9a72a28a390af11e6695ba39364422fef
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-10-16 11:25:43 +0200

    fix: syntax

--- src/informations/experiences.js
+++ src/informations/experiences.js
@@ -15,21 +15,21 @@ const experiences = [
       point: "Prototypage, conception et réalisation de sites e-commerce avec Woocommerce"
     },
      {
-      point: "Contact et suivi régulier avec les clients."
+      point: "Contact et suivi régulier avec les clients"
     }],
   },
   {
     title: "GROUPE PSA (PEUGEOT)",
     logoUrl: 'https://images.caradisiac.com/logos/4/7/3/3/194733/S0-ventes-peugeot-perce-grace-a-l-iran-112018.jpg',
     period: 'Janvier 2020– Février 2020',
-    body: [{point:"Stage dans le secteur Recherche et Développement chez PSA Poissy."},
-   { point: "Création d’un programme Excel en VBA permettant de faciliter la récupération et l’interprétation de certaines données liées aux retours utilisateurs."}],
+    body: [{point:"Stage dans le secteur Recherche et Développement chez PSA Poissy"},
+   { point: "Création d’un programme Excel en VBA permettant de faciliter la récupération et l’interprétation de certaines données liées aux retours utilisateurs"}],
   },
   {
     title: "THE CHARGING PLACE",
     period: 'Mai 2019– Juillet 2019',
 logoUrl:'https://pbs.twimg.com/profile_images/1348978435302043649/_fThc9l3_400x400.jpg',
-    body: [{point:"Manutentionnaire : installation, réparation et récupération de bornes de rechargement de portables sur plusieurs sites et pays : Roland Garros, Parc des Princes, Belgique, Pays-Bas, Normandie, etc."},{point:"Hôte : assistance des personnes qui souhaitaient mettre leurs portables à recharger, maintenance des machines pendant tout le tournoi."}]
+    body: [{point:"Manutentionnaire : installation, réparation et récupération de bornes de rechargement de portables sur plusieurs sites et pays : Roland Garros, Parc des Princes, Belgique, Pays-Bas, Normandie, etc."},{point:"Hôte : assistance des personnes qui souhaitaient mettre leurs portables à recharger, maintenance des machines pendant tout le tournoi"}]
   },
 ];
 export default experiences;
