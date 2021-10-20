a3d9aa40a8ba40e1de70f2bf5151f95ebd6b11ec
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-10-20 09:34:23 +0200

    feat: london formation

--- src/informations/formations.js
+++ src/informations/formations.js
@@ -3,6 +3,10 @@ const formations = [
       title: "MASTER INGENIERIE",
       body: "ECE Paris école d'ingénieurs, majeure systèmes d'informations (SI) section internationale. ",
       period:"2018-2023",
+    }, {
+      title: "INSEEC U London",
+      body: "Second semestre de troisième année à Inseec U London (distanciel).",
+      period:"Janvier 2021-Mai 2021",
     },
     {
       title: "BACCALAUREAT SCIENTIFIQUE (mention AB)",
